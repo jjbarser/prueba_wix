92f19bfe31ef24f07a147019ff28cdc13214bffa
jjbarser <jjbarser@gmail.com>
2020-11-06 09:36:48 -0500

    Update code_example.js
    cambios nuevos

--- code_example.js
+++ code_example.js
@@ -1,9 +1,62 @@
 class MyElement extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = 'Hello World!';
-    // Trigger an event that Corvid can watch for
-    // and handle using the [`on()`] function.
-    this.dispatchEvent(new CustomEvent('my-event'));
-  }
+
+    constructor() {
+        super();
+        this.name;
+    }
+    attributeChangeCallback (nameAtr) { 
+        this.name = nameAtr;
+        console.log(nameAtr);
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <div>
+        <label for="price" class="block text-sm leading-5 font-medium text-gray-700"
+          >Price</label
+        >
+        <div class="mt-1 relative rounded-md shadow-sm">
+          <div
+            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+          >
+            <span class="text-gray-500 sm:text-sm sm:leading-5">
+              $
+            </span>
+          </div>
+          <input
+            id="price"
+            class="form-input block w-full pl-7 pr-12 sm:text-sm sm:leading-5"
+            placeholder="0.00"
+          />
+          <div class="absolute inset-y-0 right-0 flex items-center">
+            <select
+              aria-label="Currency"
+              class="form-select h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm sm:leading-5"
+            >
+              <option>${this.name}</option>
+              <option>CAD</option>
+              <option>EUR</option>
+            </select>
+            <button
+              onclick="getPicture()"
+              class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
+            >
+              <slot></slot>
+            </button>
+          </div>
+        </div>
+      </div>
+        `;
+        this.style.color = 'blue';
+        // Trigger an event that Corvid can watch for
+        // and handle using the [`on()`] function.
+        
+    }
+
+    getPicture () {
+        this.dispatchEvent(new CustomEvent('my-event'));
+    }
+
 }
-customElements.define('my-custom-element', MyElement);
+customElements.define('custom-element', MyElement);
+
