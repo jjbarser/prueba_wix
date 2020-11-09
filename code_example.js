cb5101ae919a8bb4379d1d828ffe340e8edea5c7
jjbarser <jjbarser@gmail.com>
2020-11-09 12:12:32 -0500

    Update code_example.js

--- code_example.js
+++ code_example.js
@@ -23,26 +23,40 @@ class MyElement extends HTMLElement {
 
 
   connectedCallback() {
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({mode: 'open'});
     this.innerHTML = `
         <div>
-        <button id="button">ocultar</button>
+        <label for="price" class="block text-sm leading-5 font-medium text-gray-700"
+          >Price</label
+        >
+        <div class="mt-1 relative rounded-md shadow-sm">
+          <div
+            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+          >
+            <span class="text-gray-500 sm:text-sm sm:leading-5">
+              $
+            </span>
+          </div>
+          <input
+            id="price"
+            class="form-input block w-full pl-7 pr-12 sm:text-sm sm:leading-5"
+            placeholder="0.00"
+          />
+          <div class="absolute inset-y-0 right-0 flex items-center">
+          </div>
+        </div>
+        <button id="button">${this.name} </button>
+        <button id="button2">${this.surname} </button>
       </div>
         `;
     this.style.color = 'blue';
-    //this.dispatchEvent(new CustomEvent('my-event'));
+     //this.dispatchEvent(new CustomEvent('my-event'));
 
-    const button = document.querySelector('#button');
-    button.addEventListener("click", () => {
-      console.log('Se lanzó la alerta');
+     const button = document.querySelector('#button');
+     button.addEventListener("click", () => {
+       console.log('Se lanzó la alerta');
       this.dispatchEvent(new CustomEvent('my-event'));
-    });
-
-    const button2 = document.querySelector('#button2');
-    button2.addEventListener("click", () => {
-      console.log('Se lanzó la alerta');
-      this.dispatchEvent(new CustomEvent('my-event2'));
-    });
+     });
     //const componentNuevo = document.getElementsByTagName('recibe-element')[0];
     //componentNuevo.dato1 = 'jeison';
     //componentNuevo.dato2 = 'Julian';
@@ -51,7 +65,7 @@ class MyElement extends HTMLElement {
 
 
 
-  cambiarEfecto = () => {
+  cambiarEfecto= () => {
     alert('hola click');
     document.dispatchEvent(new CustomEvent('my-event'));
   }
