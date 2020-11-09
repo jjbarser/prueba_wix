0a7281dbd43d7ebf62fbe03573763cf5c2abbf6f
jjbarser <jjbarser@gmail.com>
2020-11-09 09:39:28 -0500

    Update code_example2.js

--- code_example2.js
+++ code_example2.js
@@ -1,41 +1,71 @@
-class RecibeElement extends HTMLElement {
+class MyElement2 extends HTMLElement {
 
-    constructor() {
-        super();
-        this.dato1;
-        this.dato2;
+  constructor() {
+    super();
+    this.name;
+    this.surname;
+  }
+
+  static get observedAttributes() {
+    return ['name', "surname"];
+  }
+
+  attributeChangedCallback(nameAttr, oldValue, newValue) {
+    switch (nameAttr) {
+      case "name":
+        this.name = newValue;
+        break;
+      case "surname":
+        this.surname = newValue;
+        break;
     }
+  }
 
-    static get observedAttributes() {
-        return ['dato1', "dato2"];
-    }
+  connectedCallback() {
+    this.innerHTML = `
+        <div>
+        <label for="price" class="block text-sm leading-5 font-medium text-gray-700"
+          >Price</label
+        >
+        <div class="mt-1 relative rounded-md shadow-sm">
+          <div
+            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+          >
+            <span class="text-gray-500 sm:text-sm sm:leading-5">
+              $
+            </span>
+          </div>
+          <input
+            id="price"
+            class="form-input block w-full pl-7 pr-12 sm:text-sm sm:leading-5"
+            placeholder="0.00"
+          />
+          <div class="absolute inset-y-0 right-0 flex items-center">
+            <select
+              aria-label="Currency"
+              class="form-select h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm sm:leading-5"
+            >
+              <option>${this.name}</option>
+              <option>${this.surname}</option>
+              <option>EUR</option>
+            </select>
+          </div>
+        </div>
+        <button onclick="getPicture()">Ejecutar Proceso
+        <strong>Venndelo</strong></button>
+      </div>
+        `;
+    this.style.color = 'blue';
+    this.dispatchEvent(new CustomEvent('my-event'));
+    const componentNuevo = document.getElementsByTagName('recibe-element')[0];
+    componentNuevo.dato1 = 'jeison';
+    componentNuevo.dato2 = 'Julian';
 
-    attributeChangedCallback(nameAttr, oldValue, newValue) {
-        switch (nameAttr) {
-            case "name":
-                this.dato1 = newValue;
-                break;
-            case "surname":
-                this.dato2 = newValue;
-                break;
-        }
-    }
+  }
 
-    connectedCallback() {
-        this.innerHTML = `
-    <div>
-    <span>mostrar dato emitido</span>
-    <strong>${this.dato1}</strong>
-    <strong>${this.dato2}</strong>
-</div>
-          `;
-        this.dispatchEvent(new CustomEvent('my-event2'));
-
-    }
-
-    getPicture() {
-        this.dispatchEvent(new CustomEvent('my-event2'));
-    }
+  getPicture() {
+    this.dispatchEvent(new CustomEvent('my-event'));
+  }
 
 }
-customElements.define('recibe-element', RecibeElement);
+customElements.define('custom-element2', MyElement2);
