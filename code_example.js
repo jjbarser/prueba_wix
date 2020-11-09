b7a256c2391c20673bf0c6b41db6024b5ee52eb1
jjbarser <jjbarser@gmail.com>
2020-11-09 08:51:36 -0500

    Update code_example.js
    ajustes

--- code_example.js
+++ code_example.js
@@ -1,67 +1,68 @@
 class MyElement extends HTMLElement {
 
-  constructor(){
-      super();
-      this.name;
-      this.surname;
+  constructor() {
+    super();
+    this.name;
+    this.surname;
   }
 
-  static get observedAttributes(){
-      return ['name', "surname"];
+  static get observedAttributes() {
+    return ['name', "surname"];
   }
-    
-   attributeChangedCallback(nameAttr, oldValue, newValue){
-      switch(nameAttr){
-          case "name":
-              this.name = newValue;
-          break;
-          case "surname":
-              this.surname = newValue;
-          break;
-      }
+
+  attributeChangedCallback(nameAttr, oldValue, newValue) {
+    switch (nameAttr) {
+      case "name":
+        this.name = newValue;
+        break;
+      case "surname":
+        this.surname = newValue;
+        break;
+    }
   }
 
   connectedCallback() {
-      this.innerHTML = `
-      <div>
-      <label for="price" class="block text-sm leading-5 font-medium text-gray-700"
-        >Price</label
-      >
-      <div class="mt-1 relative rounded-md shadow-sm">
-        <div
-          class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+    this.innerHTML = `
+        <div>
+        <label for="price" class="block text-sm leading-5 font-medium text-gray-700"
+          >Price</label
         >
-          <span class="text-gray-500 sm:text-sm sm:leading-5">
-            $
-          </span>
-        </div>
-        <input
-          id="price"
-          class="form-input block w-full pl-7 pr-12 sm:text-sm sm:leading-5"
-          placeholder="0.00"
-        />
-        <div class="absolute inset-y-0 right-0 flex items-center">
-          <select
-            aria-label="Currency"
-            class="form-select h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm sm:leading-5"
+        <div class="mt-1 relative rounded-md shadow-sm">
+          <div
+            class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
           >
-            <option>${this.name}</option>
-            <option>CAD</option>
-            <option>EUR</option>
-          </select>
+            <span class="text-gray-500 sm:text-sm sm:leading-5">
+              $
+            </span>
+          </div>
+          <input
+            id="price"
+            class="form-input block w-full pl-7 pr-12 sm:text-sm sm:leading-5"
+            placeholder="0.00"
+          />
+          <div class="absolute inset-y-0 right-0 flex items-center">
+            <select
+              aria-label="Currency"
+              class="form-select h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm sm:leading-5"
+            >
+              <option>${this.name}</option>
+              <option>CAD</option>
+              <option>EUR</option>
+            </select>
+          </div>
         </div>
+        <button onclick="getPicture()">Ejecutar Proceso
+        <strong>Venndelo</strong></button>
       </div>
-      <button onclick="getPicture()">Ejecutar Proceso
-      <strong>Venndelo</strong></button>
-    </div>
-      `;
-      this.style.color = 'blue';
-      this.dispatchEvent(new CustomEvent('my-event'));
-   
+        `;
+    this.style.color = 'blue';
+    this.dispatchEvent(new CustomEvent('my-event'));
+
   }
 
-  getPicture () {
-      this.dispatchEvent(new CustomEvent('my-event'));
+  getPicture() {
+    this.dispatchEvent(new CustomEvent('my-event'));
   }
 
 }
+customElements.define('custom-element', MyElement);
