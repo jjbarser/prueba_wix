9b1a4fc14677d37d5de39971a2d6779f06bd2890
jjbarser <jjbarser@gmail.com>
2020-11-17 15:35:04 -0500

    Update code_example2.js

--- code_example2.js
+++ code_example2.js
@@ -23,6 +23,10 @@ class MyElement2 extends HTMLElement {
     }
   
     this.connectedCallback();
+    const divModificar = document.querySelector('.mostrar');
+    divModificar.style.backgroundColor = 'red';
+    divModificar.style.height = '500px'
+
     // const componentOne = document.getElementsByTagName('custom-element2')[0];
     // const div = document.querySelectorAll('div.oculto')
     // div.forEach(data => {
@@ -33,7 +37,7 @@ class MyElement2 extends HTMLElement {
   connectedCallback() {
     //let shadowRoot = this.attachShadow({mode: 'open'});
     this.innerHTML = `
-        <div>
+        <div class="mostrar">
           <span>Recibiendo parametros</span> 
           <div style="visibility: ${this.name};" class="oculto"> Tus producto son:  ${this.name}</div>
           <div style="visibility: ${this.name};" class="oculto"> Los productos adquiridos son:   ${this.surname}</div>
