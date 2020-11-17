2ebdede89bcdfad7130463f9c25c4b3247bc5aa3
jjbarser <jjbarser@gmail.com>
2020-11-17 16:59:42 -0500

    Update code_example2.js

--- code_example2.js
+++ code_example2.js
@@ -21,12 +21,8 @@ class MyElement2 extends HTMLElement {
         break;
       }
     }
-  
-    this.connectedCallback();
-    const divModificar = document.querySelector('.mostrar');
-    divModificar.style.backgroundColor = 'red';
-    divModificar.style.height = '500px'
 
+    //this.connectedCallback();
     // const componentOne = document.getElementsByTagName('custom-element2')[0];
     // const div = document.querySelectorAll('div.oculto')
     // div.forEach(data => {
@@ -36,19 +32,26 @@ class MyElement2 extends HTMLElement {
 
   connectedCallback() {
     //let shadowRoot = this.attachShadow({mode: 'open'});
+
+
     this.innerHTML = `
-        <div class="mostrar">
+    <style>
+    .mostrar {
+      color: blue;
+      height: 500px;
+      background-color: gray;
+    }
+    </style>
+        <div>
           <span>Recibiendo parametros</span> 
           <div style="visibility: ${this.name};" class="oculto"> Tus producto son:  ${this.name}</div>
           <div style="visibility: ${this.name};" class="oculto"> Los productos adquiridos son:   ${this.surname}</div>
         </div>
           `;
-    this.style.color = 'blue';
+
+   // this.style.color = 'blue';
     //this.dispatchEvent(new CustomEvent('my-event'));
     //const componentNuevo = document.getElementsByTagName('recibe-element')[0];
-    //componentNuevo.dato1 = 'jeison';
-    //componentNuevo.dato2 = 'Julian';
-
   }
 }
 customElements.define('custom-element2', MyElement2);
